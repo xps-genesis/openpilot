ba49462f258eb6b650a3e2eed80072380208f682
xps-genesis <aruncool17@gmail.com>
2020-11-25 09:29:04 -0500

    # Conflicts: #	RELEASES.md #	apk/ai.comma.plus.offroad.apk #	cereal/car.capnp #	selfdrive/boardd/boardd.cc #	selfdrive/car/hyundai/carcontroller.py #	selfdrive/car/hyundai/hyundaican.py #	selfdrive/car/hyundai/interface.py #	selfdrive/car/hyundai/values.py #	selfdrive/common/version.h #	selfdrive/ui/SConscript #	selfdrive/ui/paint.cc #	selfdrive/ui/qt/offroad/onboarding.cc #	selfdrive/ui/qt/offroad/onboarding.hpp #	selfdrive/ui/qt/spinner.cc #	selfdrive/ui/qt/text.cc #	selfdrive/ui/qt/ui.cc #	selfdrive/ui/qt/window.cc #	selfdrive/ui/qt/window.hpp #	selfdrive/ui/ui.cc #	site_scons/site_tools/cython.py

--- selfdrive/controls/lib/planner.py
+++ selfdrive/controls/lib/planner.py
@@ -121,7 +121,7 @@ class Planner():
     lead_2 = sm['radarState'].leadTwo
 
     enabled = (long_control_state == LongCtrlState.pid) or (long_control_state == LongCtrlState.stopping)
-    following = lead_1.status and lead_1.dRel < 150.0
+    following = lead_1.status and lead_1.dRel < 45.0 and lead_1.vLeadK > v_ego and lead_1.aLeadK > 0.0
 
     # Calculate speed for normal cruise control
     if enabled and not self.first_loop and not sm['carState'].gasPressed:
