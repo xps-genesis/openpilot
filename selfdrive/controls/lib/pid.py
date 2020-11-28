eb34071710de5b4856ee046ca81b1abcf2bcf593
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-11-27 19:37:25 -0500

    Update pid.py

--- selfdrive/controls/lib/pid.py
+++ selfdrive/controls/lib/pid.py
@@ -2,7 +2,7 @@ import numpy as np
 from common.numpy_fast import clip, interp
 
 GainSaS_BP = [0., 3.9, 4., 5., 10., 20., 40.]
-Gain_g = [0.15, .044, .045, .085, .12, .14, .16]
+Gain_g = [0.15, .065, .085, .1, .12, .14, .16]
 
 GainV_BP = [0., 20., 20.01, 30.]
 Gain_V = [0.3, .5, .65, 1.2]
