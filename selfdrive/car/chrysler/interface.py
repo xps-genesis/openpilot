021084ac3e6e07896467ab17b6655850fade1c9b
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-04-09 10:03:48 -0400

    Update interface.py

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -38,11 +38,11 @@ class CarInterface(CarInterfaceBase):
       ret.lateralTuning.pid.kiBP = [0., 30.]
       ret.lateralTuning.pid.kiV = [0.02, 0.03]
 
-      ret.lateralTuning.pid.kdBP = [0. 30.]
-      ret.lateralTuning.pid.kdV = [1. 5.]
+      ret.lateralTuning.pid.kdBP = [0., 30.]
+      ret.lateralTuning.pid.kdV = [1., 5.]
 
-      ret.lateralTuning.pid.kfBP = [0. 30.]
-      ret.lateralTuning.pid.kfV = [0.00002 0.000025]   # full torque for 10 deg at 80mph means 0.00007818594
+      ret.lateralTuning.pid.kfBP = [0., 30.]
+      ret.lateralTuning.pid.kfV = [0.00002, 0.000025]   # full torque for 10 deg at 80mph means 0.00007818594
 
     ret.steerActuatorDelay = 0.01
     ret.steerRateCost = 0.4
