4b4b0c8c6918cbba58174c0bb50b4b6c02e5fbb8
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-04-10 16:17:07 -0400

    Update safety_chrysler.h

--- panda/board/safety/safety_chrysler.h
+++ panda/board/safety/safety_chrysler.h
@@ -176,10 +176,10 @@ static int chrysler_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   }
 
   // FORCE CANCEL: only the cancel button press is allowed
-  if (addr == 571) {
-    if ((GET_BYTE(to_send, 0) != 1) || ((GET_BYTE(to_send, 1) & 1) == 1)) {
-      tx = 0;
-    }
+  //if (addr == 571) {
+  //  if ((GET_BYTE(to_send, 0) != 1) || ((GET_BYTE(to_send, 1) & 1) == 1)) {
+  //    tx = 0;
+  //  }
   }
 
   return tx;
