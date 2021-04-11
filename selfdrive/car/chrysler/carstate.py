5dfa49753b46575097bdcce359bb425628981a51
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-04-11 17:25:27 -0400

    Update carstate.py

--- selfdrive/car/chrysler/carstate.py
+++ selfdrive/car/chrysler/carstate.py
@@ -76,7 +76,7 @@ class CarState(CarStateBase):
     self.button_counter = cp.vl["WHEEL_BUTTONS"]['COUNTER']
 
     self.acc_button_pressed = bool(cp.vl["WHEEL_BUTTONS"]['ACC_CANCEL']) \
-                              or bool(cp.vl["WHEEL_BUTTONS"]['ACC_RESUME']) \
+                          #    or bool(cp.vl["WHEEL_BUTTONS"]['ACC_RESUME']) \
                               or bool(cp.vl["WHEEL_BUTTONS"]['ACC_SPEED_INC']) \
                               or bool(cp.vl["WHEEL_BUTTONS"]['ACC_SPEED_DEC']) \
                               or bool(cp.vl["WHEEL_BUTTONS"]['ACC_FOLLOW_DEC']) \
