34aae88f51b9bedf4d43ae1152f5a9df42f7ad27
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-04-11 21:27:58 -0400

    Update interfaces.py

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -133,7 +133,7 @@ class CarInterfaceBase():
     # Optionally allow to press gas at zero speed to resume.
     # e.g. Chrysler does not spam the resume button yet, so resuming with gas is handy. FIXME!
     #if (cs_out.gasPressed and (not self.CS.out.gasPressed) and cs_out.vEgo > gas_resume_speed) or \
-    if not opParams().get('brakedisengageDisable'):
+    if not opParams().get('yoloMode'):
       if (cs_out.brakePressed and (not self.CS.out.brakePressed or not cs_out.standstill)):
         events.add(EventName.pedalPressed)
 
