2528b06cb2e8c7b03b0a32180c755844886f0525
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-04-18 10:54:48 -0400

    Update carcontroller.py

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -128,18 +128,16 @@ class CarController():
     # LKAS_HEARTBIT is forwarded by Panda so no need to send it here.
     # frame is 100Hz (0.01s period)
     if (self.ccframe % 2 == 0) and wp_type == 2:  # 0.02s period
-      if (CS.lkas_car_model != -1):
-        new_msg = create_apa_hud(
-            self.packer, self.apaActive, CS.apaFault, lkas_active,
-            self.steer_type)
-        can_sends.append(new_msg)
+      new_msg = create_apa_hud(
+          self.packer, self.apaActive, CS.apaFault, lkas_active,
+          self.steer_type)
+      can_sends.append(new_msg)
 
     if (self.ccframe % 2 == 0) and wp_type != 2:  # 0.25s period
-      if (CS.lkas_car_model != -1):
-        new_msg = create_lkas_hud(
-            self.packer, CS.out.gearShifter, lkas_active,
-            self.hud_count, self.steer_type)
-        can_sends.append(new_msg)
+      new_msg = create_lkas_hud(
+          self.packer, CS.out.gearShifter, lkas_active,
+          self.hud_count, self.steer_type)
+      can_sends.append(new_msg)
 
     if self.ccframe % 25 == 0:
       self.hud_count += 1
