d2a49519fbd07db3cbc51ea793376b2acec86c03
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-04-18 16:08:25 -0400

    Update values.py

--- selfdrive/car/chrysler/values.py
+++ selfdrive/car/chrysler/values.py
@@ -10,7 +10,7 @@ class CarControllerParams:
   STEER_MAX = 261  # 262 faults
   STEER_DELTA_UP = 1.4  # 3 is stock. 100 is fine. 200 is too much it seems
   STEER_DELTA_DOWN = 3  # no faults on the way down it seems
-  STEER_ERROR_MAX = 80
+  STEER_ERROR_MAX = 8000
 
 
 class CAR:
