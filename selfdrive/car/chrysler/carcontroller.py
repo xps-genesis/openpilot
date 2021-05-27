1c7efd05013508f85a6f9141f403f7cd81355fbe
xps-genesis <aruncool17@gmail.com>
2021-05-27 19:41:18 -0400

    r

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -147,7 +147,7 @@ class CarController():
         self.op_cancel_cmd = True
       elif enabled and self.resume_press and not self.op_long_enable and ((CS.lead_dist > self.lead_dist_at_stop) or (op_lead_rvel > 0) or (15 > CS.lead_dist >= 6.)):
         button_type = 'ACC_RESUME'
-      elif long_starting:
+      elif self.go_req or long_starting:
         button_type = 'ACC_RESUME'
 
       if button_type is not None:
