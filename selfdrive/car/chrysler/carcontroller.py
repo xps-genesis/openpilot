f89a3bc50d7288c87a5d2e7e99777cec45e1de11
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-06-04 17:19:54 -0400

    Update carcontroller.py

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -219,7 +219,7 @@ class CarController():
     self.decel_val = DEFAULT_DECEL
     self.trq_val = CS.axle_torq_min
 
-    apply_accel = (actuators.gas - actuators.brake) if enabled else 0.
+    apply_accel = (actuators.gas - actuators.brake - CS.hill_accel) if enabled else 0.
 
     accmaxBp = [20, 25, 40]
     if Params().get_bool('ChryslerMadGas'):
@@ -249,7 +249,7 @@ class CarController():
             (self.stop_req or (apply_accel <= min((CS.axle_torq_min - 20.)/CV.ACCEL_TO_NM, START_BRAKE_THRESHOLD))
              or (self.decel_active and not self.stop_req and (CS.out.brake > 10. or CS.hybrid_power_meter < 0.))):
       self.decel_active = True
-      self.decel_val = apply_accel - CS.hill_accel
+      self.decel_val = apply_accel
       #if self.decel_val_prev > self.decel_val and not self.done:
       #  self.decel_val = accel_rate_limit(self.decel_val, self.decel_val_prev)
       #else:
@@ -260,7 +260,7 @@ class CarController():
       #self.done = False
       #self.decel_val_prev = CS.out.aEgo
 
-    if enabled and not CS.out.brakePressed and (self.decel_active or self.go_req) and\
+    if enabled and not CS.out.brakePressed and (not self.decel_active or self.go_req) and\
             (apply_accel >= max(START_GAS_THRESHOLD, CS.axle_torq_min/CV.ACCEL_TO_NM)
              or self.accel_active and apply_accel > CS.axle_torq_min/CV.ACCEL_TO_NM):
       self.trq_val = apply_accel * CV.ACCEL_TO_NM
