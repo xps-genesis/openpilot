f5182008e5384b4ea03c9d9922f28d09c26f8b6e
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-06-08 14:33:04 -0400

    Update dmonitoringd.py

--- selfdrive/monitoring/dmonitoringd.py
+++ selfdrive/monitoring/dmonitoringd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from cereal import car
-from common import op_params
+from common.op_params import opParams
 from common.params import Params
 import cereal.messaging as messaging
 from selfdrive.controls.lib.events import Events
