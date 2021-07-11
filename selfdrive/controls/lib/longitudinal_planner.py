9376a63f5dd718a7b3954936d96ab42b3d18797b
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2021-07-11 14:55:05 -0400

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -18,8 +18,8 @@ from selfdrive.swaglog import cloudlog
 
 LON_MPC_STEP = 0.2  # first step is 0.2s
 AWARENESS_DECEL = -0.2     # car smoothly decel at .2m/s^2 when user is distracted
-A_CRUISE_MIN = -0.3
-A_CRUISE_MAX = 1.2
+A_CRUISE_MIN = -0.5
+A_CRUISE_MAX = 2.0
 
 # Lookup table for turns
 _A_TOTAL_MAX_V = [1.7, 3.2]
